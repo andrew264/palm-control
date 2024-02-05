4cd79af3cd3ddd82e741971dbfb9ed0a6302f8a5
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-02-05 15:22:16 +0530

    added Kalman Filtering

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 
 import cv2
 import mediapipe as mp
+import numpy as np
 import requests
 from mediapipe.tasks import python
 
-from utils import HAND_CONNECTIONS, _landmarks_list_to_array
+from utils import HAND_CONNECTIONS, _landmarks_list_to_array, HandLandmarkKalmanFilter, _rescale_landmarks
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -24,11 +25,6 @@ def get_hand_landmark_model():
 
 
 def draw_landmarks_on_image(image, landmark_point: list):
-    if not landmark_point:
-        return image
-
-    landmark_point = _landmarks_list_to_array(landmark_point, image.shape)
-
     colors = [(0, 0, 0), (255, 255, 255)]
 
     def draw_line(start, end, thickness):
@@ -51,11 +47,12 @@ def draw_landmarks_on_image(image, landmark_point: list):
 if __name__ == '__main__':
     options = HandLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=get_hand_landmark_model()),
-        num_hands=2,
+        num_hands=1,
         min_hand_detection_confidence=0.6,
         min_hand_presence_confidence=0.6,
         min_tracking_confidence=0.5,
         running_mode=VisionRunningMode.VIDEO, )
+    kalman_filter = HandLandmarkKalmanFilter(initial_state=np.zeros(42))
     with HandLandmarker.create_from_options(options) as landmarker:
         cap = cv2.VideoCapture(0)
         timestamp = 0
@@ -68,7 +65,9 @@ if __name__ == '__main__':
             hand_landmarker_result = landmarker.detect_for_video(mp_image, timestamp)
             timestamp += 1
             for hand_landmarks in hand_landmarker_result.hand_landmarks:
-                image = draw_landmarks_on_image(image, hand_landmarks)
+                smoothened_points = kalman_filter.update(_landmarks_list_to_array(hand_landmarks))
+                rescaled_points = _rescale_landmarks(smoothened_points, image.shape)
+                image = draw_landmarks_on_image(image, rescaled_points)
             cv2.imshow('MediaPipe Hand Landmarks', image)
             if cv2.waitKey(5) & 0xFF == 27:
                 break
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import enum
 
+import numpy as np
+from filterpy.kalman import KalmanFilter
+
 HAND_PALM_CONNECTIONS = ((0, 1), (0, 5), (9, 13), (13, 17), (5, 9), (0, 17))
 
 HAND_THUMB_CONNECTIONS = ((1, 2), (2, 3), (3, 4))
@@ -44,6 +47,28 @@ class HandLandmark(enum.IntEnum):
     PINKY_TIP = 20
 
 
-def _landmarks_list_to_array(landmark_list, image_shape):
+def _landmarks_list_to_array(landmark_list) -> np.ndarray:
+    return np.array([[point.x, point.y] for point in landmark_list])
+
+
+def _rescale_landmarks(landmarks: np.ndarray, image_shape) -> list:
     rows, cols, _ = image_shape
-    return [[int(point.x * cols), int(point.y * rows)] for point in landmark_list]
+    return [[int(point[0] * cols), int(point[1] * rows)] for point in landmarks]
+
+
+class HandLandmarkKalmanFilter:
+    def __init__(self, initial_state):
+        self.kf = KalmanFilter(dim_x=42, dim_z=21 * 2)
+        self.kf.F = np.eye(42)
+        self.kf.H = np.eye(21 * 2)
+        self.kf.Q *= 8e-2
+        self.kf.R *= 1e-1
+        self.kf.x = np.reshape(initial_state, (42, 1))
+        self.kf.P *= 1e-1
+
+    def update(self, measurement: np.ndarray) -> np.ndarray:
+        self.kf.predict()
+        if measurement.ndim > 1:
+            measurement = measurement.flatten()
+        self.kf.update(measurement)
+        return self.kf.x.reshape(-1, 2)
